64479f3a273a467519a2141cd69c0211a3f68bcb
gabriel tavares <gabriel29t@gmail.com>
2022-03-01 10:48:59 -0300

    add more films to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ Movie.destroy_all
 puts "Database cleaned"
 
 url = "http://tmdb.lewagon.com/movie/top_rated"
-10.times do |i|
-  puts "Importing movies from page #{i + 1}"
-  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+20.times do |i|
+  puts "Importing movies from page #{i + 11}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 11}").read)['results']
   movies.each do |movie|
     puts "Creating #{movie['title']}"
     base_poster_url = "https://image.tmdb.org/t/p/original"
